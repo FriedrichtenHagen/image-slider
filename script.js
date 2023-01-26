4e72112d509e08add4c964ea702346e00eafb253
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-26 22:06:13 +0100

    add attempts

---
+++ script.js
@@ -0,0 +1,6 @@
+const slide1 = document.querySelector("#slide1")
+const slide2 = document.querySelector("#slide2")
+const slide3 = document.querySelector("#slide3")
+const slide4 = document.querySelector("#slide4")
+
+// https://alvarotrigo.com/blog/javascript-slider-how-to-make-one/
